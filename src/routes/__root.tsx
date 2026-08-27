37b1d47373c6ea2ba1c66b9c4a76adbd0e365e30
Mark Adrian Remigio <remigiomark42@gmail.com>
2026-08-27 03:21:59 +0000

    Changes
    Co-authored-by: Remigio2004 <193454647+Remigio2004@users.noreply.github.com>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -134,8 +134,11 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
-      <Outlet />
+      <VaultProvider>
+        {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
+        <Outlet />
+        <Toaster />
+      </VaultProvider>
     </QueryClientProvider>
   );
 }
